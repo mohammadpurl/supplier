11bbd3a277dbdb3e9ed06c2f3bcd9df61ef30a11
leila-P <leilapoormasti@gmail.com>
2025-05-11 17:37:40 +0330

    add types for components

--- src/app/_components/comment/comment.types.ts
+++
@@ -1,4 +0,0 @@
-import { Comment } from "@/types/comment.interface";
-import { ComponentBase } from "../types/component-base.type";
-
-export type CommentProps = Omit<ComponentBase, "isDisabled" | "size"> & Comment;
--- src/app/_components/feature/feature.types.ts
+++
@@ -1,3 +0,0 @@
-export type FeatureProps = {
-    feature: Feature
-}
